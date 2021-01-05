a333104eec8b7654c7ceb751f9f8d775d8d50f51
Johnsonrobot <b10413157@yuntech.edu.tw>
2021-01-05 20:54:54 +0800

    string modify

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   session_start();
   require_once 'header.php';
 
-  echo "<div class='center'>Welcome to Master";
+  echo "<div class='center'>Welcome to Guest";
 
   if ($loggedin) echo " $user, you are logged in";
   else           echo ' please sign up or log in';
